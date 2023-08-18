1767008e89be8622c416e84d31f5ae3db055e819
Asutosh Paul <59476826+ashut5959@users.noreply.github.com>
2023-08-18 13:20:06 +0530

    (auth using clerk)

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,10 @@
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full">
+        <NavBar />
+      <main className="md:pl-20 pt-16 h-full">{children}</main>
+    </div>
+  );
+};
+
+export default RootLayout;
